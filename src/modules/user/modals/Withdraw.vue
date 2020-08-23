ff1a5d5f3008ec7f335d4eb378f851cb293599b7
the binary digit zero knowledge electronic currency bzc bitzec <54347877+bitcoino@users.noreply.github.com>
2020-08-23 05:53:44 +0200

    up

--- src/modules/user/modals/Withdraw.vue
+++ src/modules/user/modals/Withdraw.vue
@@ -71,7 +71,7 @@ export default {
         let utxos = await this.$avax.getUTXOs(myAddresses);
 
         let assetid = this.asset.asset;
-        if (this.asset.asset == "AVA") {
+        if (this.asset.asset == "AVAX") {
           assetid = "21d7KVtPrubc5fHr6CGNcgbUb4seUjmZKr35ZX7BZb5iP8pXWA";
         }
         console.log(this.asset.denomination)
